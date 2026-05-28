718c7fcae97787c9c3c4dceede0eb4eaa7316b3b
Prachi-Gupta2808 <prachig2808@gmail.com>
2026-05-28 12:40:28 +0530

    added automated retries for failed jobs

--- backend/src/config/bullmq.ts
+++ backend/src/config/bullmq.ts
@@ -3,6 +3,10 @@ import { connectionOptions } from "./redis";
 
 export const assignmentQueue = new Queue("assignment-generation", {
   connection: connectionOptions,
+  defaultJobOptions: {
+    attempts: 3,
+    backoff: { type: "exponential", delay: 2000 },
+  },
 });
 
 console.log("BullMQ Queue Ready!");
